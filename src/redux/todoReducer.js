ecc2aea86d54256a1a920fb36ba8fb12ee722f88
admrts <alidemirtas.7@icloud.com>
2023-07-01 23:11:24 +0300

    🎉

--- src/redux/todoReducer.js
+++ src/redux/todoReducer.js
@@ -1,14 +1,16 @@
+import { ActionTypes } from "./contans";
+
 const initialState = {
   todos: [],
 };
 
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_TODO":
+    case ActionTypes.ADD_TODO:
       return {
         todos: [...state.todos, action.payload],
       };
-    case "DELETE_TODO":
+    case ActionTypes.DELETE_TODO:
       return {
         todos: state.todos.filter((todo) => todo.id !== action.payload),
       };
